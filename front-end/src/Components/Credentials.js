ac127ae39d54e98da6fcb7d1a528222093fff541
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2023-10-25 20:29:03 +0530

    Update Credentials.js

--- front-end/src/Components/Credentials.js
+++ front-end/src/Components/Credentials.js
@@ -1,8 +1,8 @@
 const Credentials = () => {
 
     return {
-        ClientId: '6f4420b4923c4b598f9c2954e56ff9d8',
-        ClientSecret: 'c73bbe885e854d9d875cd514d5ba4e7a'
+        ClientId: 'Your ClientId',
+        ClientSecret: 'Your ClientSecret'
     }
 }
 
